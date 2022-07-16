76c18d918b898ede72aaf6f54d5a7b8f8d95a4ac
AZANIR <eleoneks@gmail.com>
2022-07-16 18:51:30 +0300

    1

--- cypress/e2e/tests/myAccount.test.ts
+++ cypress/e2e/tests/myAccount.test.ts
@@ -2,7 +2,8 @@ import { loginPage } from "../pages/loginPage";
 
 describe('My Account Functionality', () => {
     beforeEach(() => {
-        loginPage.launchApplication()
+        //loginPage.launchApplication()
+        cy.visit('https://google.com');
     })
     it('Sample Test', () => {
         console.log("This is a sample test")
